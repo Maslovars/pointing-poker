2916f688212023e0f653cdcd1b7580c846f6e5be
Maslovars <maslovars@gmail.com>
2021-09-04 14:24:29 +0300

    feat: add routing

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Switch } from 'react-router';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Game from './components/game/Game';
+import Lobby from './components/lobby/Lobby';
+import Settings from './components/settings/Settings';
+import Welcome from './components/welcome/Welcome';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HI!</h1>
+      <Switch>
+        <Route exact path="/" render={() => <Welcome />}></Route>
+        <Route path="/lobby" render={() => <Lobby />} ></Route>
+        <Route path="/settings" render={() => <Settings />} ></Route>
+        <Route path="/game" render={() => <Game />} ></Route>
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/lobby/Lobby.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+const Lobby = () => {
+
+    const history = useHistory();
+    return (
+        <div>Lobby
+            <button onClick={() => history.push("settings")}>btn</button>
+        </div>
+
+    )
+
+}
+
+export default Lobby;
