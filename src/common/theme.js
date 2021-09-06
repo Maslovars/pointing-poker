89c79349a26808ba5f6fb464f0a61666f070310d
Maslovars <maslovars@gmail.com>
2021-09-06 22:29:53 +0300

    feat: add theme.js

---
+++ src/common/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+        dark_blue: "#2B3A67",
+        light_blue: "#496A81",
+        turquoise: "#66999B",
+        green: "#60DABF",
+        gray: "#C4C4C4",
+        black: "#000000",
+        white: "#FFFFFF"
+    }
+}
