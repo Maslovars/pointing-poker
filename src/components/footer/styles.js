5869db38d9a9b7419a15fdc29b05cdee4dfa1a99
Siarhei Khonski <armonius@mail.ru>
2021-09-08 11:28:11 +0300

    refactor: delete color styles from props

--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 import { theme } from '../../common/theme';
 
 export const StyledFooter = styled.div`
-  background-color: ${props => props.background || theme.colors.dark_blue};
+  background-color: ${ theme.colors.dark_blue };
   width: ${ props => props.width || '99vw' };
   height: ${ props => props.height || '8vh' };
   position: absolute;
   bottom: 0;
   left: .5vw;
-  color: ${props => props.color || theme.colors.white};
+  color: ${ theme.colors.white };
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -16,12 +16,12 @@ export const StyledFooter = styled.div`
 export const StyledLink = styled.a`
   display: block;
   font-size: 1.5vmin;
-  color: ${ theme.colors.dimgray};
+  color: ${ theme.colors.dimgray };
   text-decoration: none;
   margin: 0 2%;
   transform: rotate(-15deg);
   &:hover {
-    color: ${ theme.colors.white};
+    color: ${ theme.colors.white };
   }
 `
 export const MentorLink = styled(StyledLink)`
@@ -68,7 +68,7 @@ export const StyledContainerTitle = styled.p`
   display: block;
   font-size: 2vh;
   position: absolute;
-  background-color: ${ props => props.background || theme.colors.dark_blue};
+  background-color: ${ theme.colors.dark_blue};
   min-width: 15%;
   left: 40%;
   margin: 0;
