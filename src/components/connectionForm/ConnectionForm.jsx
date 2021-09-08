e09b67518220fc06176805fdb19c58e24c26ed60
artur <me@zalart.com>
2021-09-08 22:15:51 +0300

    feat: implement socket room connection

--- src/components/connectionForm/ConnectionForm.jsx
+++ src/components/connectionForm/ConnectionForm.jsx
@@ -1,8 +1,16 @@
 import { useFormik } from "formik";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Switch from 'react-input-switch';
 import Button from "../button/Button";
 import Input from "../input/Input";
+import { socket } from "../../common/utils/socket/socket";
+import { 
+    JOIN_ROOM, 
+    RECEIVE_MESSAGE, 
+    SEND_MESSAGE, 
+    USER_CONNECTED, 
+    USER_DISCONNECTED, 
+    ROOM_ID } from '../../common/utils/socket/constants';
 
 const ConnectionForm = () => {
 
@@ -16,11 +24,53 @@ const ConnectionForm = () => {
             ava: "",
             // isObserver: "no"
         },
-        onSubmit: values => console.log(values)
+        onSubmit: values => {
+            joinSocketRoom(values);
+        }
 
     })
 
-    console.log();
+    const handleUserConnect = (userId) => {
+        //handleIncomingMessage({ message: `${userId} connected` })
+      }
+    
+      const handleUserDisconnect = (userId) => {
+       // handleIncomingMessage({ message: `${userId} disconnected` })
+      }
+
+    const joinSocketRoom = (userData) => {
+
+        if (userData) { socket.emit(JOIN_ROOM, {
+          room: userData.room || ROOM_ID,
+          id: socket.id,
+          //убрать id
+          firstName: userData.firstName,
+          lastName: userData.lastName
+
+        })
+      }
+
+    }
+
+    const addSocketListeners = () => {
+        //socket.on(RECEIVE_MESSAGE, handleIncomingMessage)
+        socket.on('SHOW_USERS', (data) => { console.log(data)})
+        socket.on(USER_CONNECTED, handleUserConnect)
+        socket.on(USER_DISCONNECTED, handleUserDisconnect)
+      }
+
+      const removeSocketListeners = () => {
+        //socket.off(RECEIVE_MESSAGE, handleIncomingMessage)
+        socket.off(USER_CONNECTED, handleUserConnect)
+        socket.off(USER_DISCONNECTED, handleUserDisconnect)
+      }
+
+      useEffect(() => {
+        joinSocketRoom();
+        addSocketListeners();
+        return () => removeSocketListeners();
+      }, [])
+
 
     return (
         <div>
@@ -66,11 +116,12 @@ const ConnectionForm = () => {
                         type="file"
 
                     />
-                    <Button text="button" />
+                    <Button type="button" text="Button" />
                 </div>
                 <Button text="Confirm" height="big" type="submit" />
                 <Button color="white" text="Cancel" height="big" />
             </form>
+            
         </div>
     )
 }
