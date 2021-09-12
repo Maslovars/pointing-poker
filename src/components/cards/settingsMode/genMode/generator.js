907d40481dfcce909d56cde7fcbf440eb31b784f
Siarhei Khonski <armonius@mail.ru>
2021-09-12 20:02:49 +0300

    refactor: refactor cards apply eslint and prettier

--- src/components/cards/settingsMode/genMode/generator.js
+++ src/components/cards/settingsMode/genMode/generator.js
@@ -1,13 +1,25 @@
 import { fiboNum, power2, genMethod, CardType } from "../../constants";
 
-export function cardGenerator({name, method, num}) {
+export default function cardGenerator({ name, method, num }) {
   const array = [];
   let sample = [];
-  method === genMethod.fibo ? sample = fiboNum :
-  method === genMethod.two ? sample = power2: sample = [{ name: 'gen ERROR', type: CardType.playCard }];
-  if (!name) { name = method }
+  let newName = "";
+  if (method === genMethod.fibo) {
+    sample = fiboNum;
+  } else if (method === genMethod.two) {
+    sample = power2;
+  } else {
+    sample = [{ name: "gen ERROR", type: CardType.playCard }];
+  }
+  if (!name) {
+    newName = method;
+  }
   for (let i = 1; i <= num; i += 1) {
-    array.push({ type: CardType.playCard, name, value: sample[i - 1]})
-  } 
+    array.push({
+      type: CardType.playCard,
+      name: newName,
+      value: String(sample[i - 1]),
+    });
+  }
   return array;
 }
