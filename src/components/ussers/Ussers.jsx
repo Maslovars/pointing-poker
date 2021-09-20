85a11b2452ed335c538faff0dc2eefda01450ac0
Siarhei Khonski <armonius@mail.ru>
2021-09-20 21:12:35 +0300

    feat: fill

--- src/components/ussers/Ussers.jsx
+++ src/components/ussers/Ussers.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UsersContainer, UserWrapper, LeaveButton, Wrapper, UserContainer } from './style';
+import { UsersContainer, UserWrapper, LeaveButton, Wrapper, UserContainer, StyledLinkContainer, StyledInput, InputsContainer, StartButton } from './style';
 import User from './user/User';
 import { useSelector } from 'react-redux';
 import { socket } from '../../common/utils/socket/socket';
@@ -7,22 +7,33 @@ import { LEAVE_GAME } from '../../common/utils/socket/constants';
 import { useLocation, Redirect } from 'react-router-dom';
 
 export default function Ussers() {
+  const url = 'http://SITE_NAME' + useLocation().pathname;
   const users = useSelector((state) => state.appState.users);
   const userId = socket.id;
   const userData = users.find((user) => user.userId === userId);
   const gameId = useLocation().pathname.replace('/lobby/', '');
   
   const leaveHandler = () => { socket.emit(LEAVE_GAME, { gameId, userId }) };
-
-  console.log('USERS>>>', users)
-
+  const copyHandler = () => {const link = document.getElementById('link');
+    link.select();
+    document.execCommand('copy')
+  };
+  
   return <Wrapper>
     { !userData && <Redirect to='/' /> }
     <UserContainer>
       <UserWrapper>
-      { !!userData && <User id={userData.userId} name={userData.firstName} surname={userData.lastName} position={userData.position} ava={userData.ava} master={true} /> }
+        { !!userData && <User id={userData.userId} name={userData.firstName} surname={userData.lastName} position={userData.position} ava={userData.ava} master={true} /> }
+        { !!userData && userData.isMaster && <StyledLinkContainer>
+          <p>link to lobby:</p>
+        <InputsContainer>
+          <StyledInput id='link' width='350px' type='text' readOnly={true} value={url} />
+          <StyledInput width='75px' type='button' value='COPY' onClick={copyHandler} />
+        </InputsContainer>
+        <StartButton type='start' value='START NEW GAME'  />
+        </StyledLinkContainer> }
       </UserWrapper>
-      <LeaveButton type='button' value='LEAVE LOBBY' onClick={leaveHandler}/>
+      <LeaveButton type='button' value='LEAVE LOBBY' onClick={leaveHandler} />
     </UserContainer>
     <UsersContainer>
       { users.map(user => user.userId !== userId && <User key={user.userId} id={user.userId} name={user.firstName} surname={user.lastName} position={user.position} ava={user.ava} master={user.isMaster} />) }
--- src/components/ussers/style.js
+++ src/components/ussers/style.js
@@ -22,20 +22,60 @@ export const UsersContainer = styled.div`
 `;
 export const UserContainer = styled.div`
   margin: 0;
-  height: 15vh;
+  height: 250px;
   width: 70%;
   min-width: 480px;
   display: flex;
   align-items: top;
   justify-content: space-between;
+  flex-wrap: wrap;
   margin-bottom: 20px;
 `;
 export const UserWrapper = styled.div`
   align-self: top;
   margin-top: 10px; 
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: top;
+`;
+export const StyledLinkContainer = styled.div`
+  display: flex;
+  min-width: 480px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: top;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-left: 10px;
+  margin-top: 20px;
+  & p {
+    margin: 0;
+  } 
+`;
+export const StyledInput = styled.input`
+  height: 30px;
+  min-width: 100px;
+  width: ${props => props.width || '200px'};
+  font-size: 18px;
+  line-height: 30px;
+  font-weight: bold;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+  ${ props => { if (props.type === 'button') { return `{ background-color: ${theme.colors.lime}; opacity: .3; cursor: pointer} &:hover { opacity: 1 };` } } }
+`;
+export const InputsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  min-width: 200px;
 `;
 export const LeaveButton = styled.input`
   background-color: ${theme.colors.red};
+  border-radius: 7px;
   color: ${theme.colors.white};
   align-self: flex-end;
   width: 180px;
@@ -52,3 +92,13 @@ export const LeaveButton = styled.input`
     opacity: 1;
   } 
 `;
+export const StartButton = styled(LeaveButton)`
+  background-color: ${theme.colors.dark_blue};
+  width: 180px;
+  height: 40px;
+  font-size: 18px;
+  opacity: .5;
+  margin-top: 20px;
+  text-align: center;
+  align-self: flex-start;
+`
